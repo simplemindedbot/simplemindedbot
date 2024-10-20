1e75f04aee0cb2b5743cfe730e0a2ea063fa6534
Scot Campbell <sr4001@gmail.com>
2024-10-20 17:22:53 -0400

    updated script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-curl -fsSL https://github.com/getzola/zola/releases/latest/download/zola-x86_64-unknown-linux-gnu.tar.gz | tar -xvz
+curl -fsSL https://github.com/getzola/zola/releases/download/v0.19.2/zola-v0.19.2-x86_64-unknown-linux-gnu.tar.gz | tar -xvz
 mv zola /usr/local/bin/
 zola build
