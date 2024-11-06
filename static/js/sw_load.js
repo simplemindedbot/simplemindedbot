9318e8b910ae4d74e1354765f91cfd31575d9c4e
Scot Campbell <sr4001@gmail.com>
2024-11-06 13:09:55 -0500

    orange

--- static/js/sw_load.js
+++ static/js/sw_load.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-        .register("/sw.min.js?v=3.12.5",
+        .register("/sw.min.js?v=4.0.0",
                   { scope: "/" })
         .then(() => {
             console.info("SW Loaded");
