7e3daaef637c0490055679e89b6ee5b6bec94526
Scot Campbell <sr4001@gmail.com>
2024-11-07 17:31:50 -0500

    pwa cache version bump and new glitch shortcodes

--- static/js/sw_load.js
+++ static/js/sw_load.js
@@ -1,6 +1,6 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-        .register("/sw.min.js?v=4.0.1",
+        .register("/sw.min.js?v=4.0.2",
                   { scope: "/" })
         .then(() => {
             console.info("SW Loaded");
